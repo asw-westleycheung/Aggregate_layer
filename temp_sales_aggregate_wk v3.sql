260f2749dfb7c2ab612030d1e9c08b373cff9472
asw-westleycheung <45157641+asw-westleycheung@users.noreply.github.com>
2019-04-25 17:13:41 +0800

    Update temp_sales_aggregate_wk v3.sql

--- temp_sales_aggregate_wk v3.sql
+++ temp_sales_aggregate_wk v3.sql
@@ -1,5 +1,4 @@
 spark.sql("""
-------3.6 mins-------
 SELECT 
      i.product_key
     ,i.store_key
@@ -21,10 +20,10 @@ SELECT
     ,SUM(CASE WHEN inventory_adjustment_reason_type = '22' AND inventory_adjustment_reason_code IN ('-10','-6','-2','7','8','54','74','75','76','77','80','81','82','83','84','85','86','87','88','89','90','91','92','93','204','205','207','208','210','211','212') THEN inventory_adjustment_retail_local_amount ELSE 0 END) AS sales_amt_local_shrink
     ,SUM(CASE WHEN inventory_adjustment_reason_type = '22' AND inventory_adjustment_reason_code IN ('-10','-6','-2','7','8','54','74','75','76','77','80','81','82','83','84','85','86','87','88','89','90','91','92','93','204','205','207','208','210','211','212') THEN inventory_adjustment_cost_local_amount ELSE 0 END) AS cogs_amt_local_shrink
     ,SUM(CASE WHEN inventory_adjustment_reason_type = '22' AND inventory_adjustment_reason_code IN ('-10','-6','-2','7','8','54','74','75','76','77','80','81','82','83','84','85','86','87','88','89','90','91','92','93','204','205','207','208','210','211','212') THEN inventory_adjustment_quantity ELSE 0 END) AS qty_shrink
-	,current_timestamp as table_last_update
-FROM rkrdmx.f_rk_inventory_adjustment i
-JOIN default.bl_business_date d ON i.transaction_date_key = d.fiscal_date_key
-where d.fiscal_week_end_date <= d.max_fiscal_date
+    ,current_timestamp as table_last_update
+FROM rdm.f_inventory_adjustment i
+JOIN default.bl_business_date d ON i.date_key = d.fiscal_date_key
+where d.fiscal_week_end_date <= date_add(d.max_fiscal_date,1)
 and (case when COALESCE(i.inventory_adjustment_retail_local_amount,0) <> 0 then 1
         when COALESCE(i.inventory_adjustment_cost_local_amount,0) <> 0 then 1
         when COALESCE(i.inventory_adjustment_quantity,0) <> 0 then 1 ELSE 0 END) = 1
